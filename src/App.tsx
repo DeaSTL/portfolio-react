01f4448aaba899424b87ce184433e9104d980fed
Jon-Michael Hartway <jhartway99@gmail.com>
2024-03-16 15:01:41 -0400

    added stars

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,27 @@ import { Analytics } from "@vercel/analytics/react"
 import ProjectSection from './components/ProjectSection'
 import ExperienceSection from './components/ExperienceSection'
 import Timeline from './components/Timeline'
+import StarController from './components/StarController'
 
 function App() {
 
   return (
     <>
+      <StarController/>
       <Analytics/>
       <div className="mx-auto max-w-screen-xl text-gray-300 md:justify-center lg:py-10">
+        <span className="static flex justify-center top-10">
+          <SocialLink icon={faGithub}  url="https://github.com/DeaSTL"/>
+          <SocialLink icon={faLinkedin}  url="https://www.linkedin.com/in/jon-michael-h/"/>
+          <SocialLink icon={faTwitter}  url="https://twitter.com/0xdeafc0de"/>
+          <SocialLink icon={faBlog}  url="https://jonhartway.blog/"/>
+        </span>
         <div className="flex flex-col md:flex-row duration-75">
           <div className="md:w-full my-10 mx-10 lg:w-1/2">
             <p className="text-4xl font-semibold p-1 hover:text-glow duration-200">Jon-Michael Hartway </p>
             <p className="text-xl font-bold text-gray-400 p-1">Software Engineer</p>
             <p className="text-lg font-light text-gray-400">Building high performance, rock-solid applications!</p>
 
-            <div className="md:py-40 py-10">
-              <SocialLink icon={faGithub}  url="https://github.com/DeaSTL"/>
-              <SocialLink icon={faLinkedin}  url="https://www.linkedin.com/in/jon-michael-h/"/>
-              <SocialLink icon={faTwitter}  url="https://twitter.com/0xdeafc0de"/>
-              <SocialLink icon={faBlog}  url="https://jonhartway.blog/"/>
-            </div>
 
             <div>
               <p className="category-header">About</p>
---
+++ src/components/StarController.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+
+type Props = {
+  starCount?:number
+}
+
+type Star = {
+  id:number
+  left:string
+  top:string
+  size:string
+  animationDuration:string
+}
+
+export default function StarController({starCount = 100}: Props) {
+  
+  const [stars, setStars] = useState<Star[]>([])
+  useEffect(() => {
+
+    const newStars = Array.from<Star[]>({ length: starCount }).map(():Star => {
+      return {
+        id:Math.random(),
+        left: `${Math.random() * 100}%`,
+        top:`${Math.random() * 400}vh`,
+        size: `${Math.random() * 2 + 1}px`,
+        animationDuration: `${Math.random() * 4 + 3}s`
+      }
+    })
+
+    setStars(newStars)
+  }, [starCount])
+  return (
+    <>
+      {stars.map(star => (
+        <span
+          key={star.id}
+          className="star"
+          style={{
+            left: star.left,
+            top: star.top,
+            width: star.size,
+            height: star.size,
+            animationDuration: star.animationDuration
+          }}
+        >
+      </span>
+      ))}
+    </>
+  )
+}
